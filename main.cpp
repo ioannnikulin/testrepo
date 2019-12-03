5c6e84a92fc37aceb5a9146f84284d6f6aba22f1
ioann <no@no.no>
2019-12-03 20:18:21 +0300

    plan

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    /**
+    1. завести переменные max_odd max_even
+    2. ввести число с клавиатуры в current_number
+    3. если current_number нечётное и большое, запомнить его в max_odd
+    4. Если чётное и большое, запомнить в max_even
+    5. повторять ввод и проверку для всех чисел
+    6. после проверки всех чисел вывести на экран сумму max_odd и max_even
+    **/
     return 0;
 }
